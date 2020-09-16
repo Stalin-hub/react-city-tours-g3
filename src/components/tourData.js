48c4c56f31ca222226c69bfbb45924c38b0307a0
Stalin-hub <stalin0112333@gmail.com>
2020-09-16 14:53:17 -0500

    change more citys

--- src/components/tourData.js
+++ src/components/tourData.js
@@ -39,5 +39,49 @@ export const tourData = [
             "Es la capital de la provincia del Azuay, está situada en la parte meridional de"+
             " la cordillera andina ecuatoriana. Su Centro Histórico fue declarado Patrimonio "+
             "de la Humanidad por la Unesco en el año 1999."
+    },
+    {
+        id: 5,
+        city: "Baños",
+        img: "./img/banos.jpg",
+        name: "Tour por Baños de Agua Santa",
+        info: 
+            "Ciudad ecuatoriana cerca del volcán activo Tungurahua, termas, cascadas y la cuenca del Amazonas."+
+            "Es conocida por su acceso a senderos en el volcán activo Tungurahua al sur y por sus aguas termales"+
+            " ricas en minerales. En el centro, la Iglesia de Nuestra Señora del Agua Santa tiene pinturas que"+
+            " representan milagros atribuidos a la Virgen María."
+    },
+    {
+        id: 6,
+        city: "Isla Santa Cruz",
+        img: "./img/islaSantaCruz.jpg",
+        name: "Tour por la estación Científica Charles Darwin",
+        info: 
+            "Santa Cruz es una isla del Parque Nacional Galápagos de Ecuador. Es conocida por las playas de "+
+            "arena como El Garrapatero. La ciudad de Puerto Ayora es el núcleo turístico del archipiélago. Aquí,"+
+            " la Estación Científica Charles Darwin tiene un programa de cría para las tortugas gigantes en "+
+            "peligro de extinción, que viven en un entorno silvestre en la Reserva El Chato."
+    },
+    {
+        id: 7,
+        city: "Otavalo",
+        img: "./img/otavalo.jpg",
+        name: "Tour por el Museo Tejidos el Obraje",
+        info: 
+            "Otavalo es una ciudad en el altiplano andino de la provincia de Imbabura, al norte de Ecuador. "+
+            "Está rodeada de volcanes, como el volcán Imbabura. Es conocida por su mercado de Otavalo en la central"+
+            " Plaza de Ponchos, donde los lugareños vestidos con ropa indígena tradicional venden textiles coloridos"+
+            " y artesanías."
+    },
+    {
+        id: 8,
+        city: "Mindo",
+        img: "./img/mindo.jpg",
+        name: "Tour por la reserva Mindo-Nambillo",
+        info: 
+            "Mindo es un poblado de la cordillera de los Andes en el norte de Ecuador. Es conocido por sus numerosas "+
+            "especies de aves, mariposas y orquídeas que habitan en el bosque nuboso circundante, parte de la Reserva"+
+            " Mindo-Nambillo. Una tarabita (teleférico) cruza el río Nambillo hacia una cima, donde hay senderos que "+
+            "llegan a varias cascadas, incluida la Cascada Nambillo."
     }
 ]
