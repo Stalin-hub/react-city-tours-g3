e9b5ab10f723e4ba1eabf07359a3f3439d574ddf
Stalin-hub <stalin0112333@gmail.com>
2020-09-16 12:33:35 -0500

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,43 @@
+export const tourData = [
+    {
+        id: 1,
+        city: "Quito",
+        img: "./img/quito.jpg",
+        name: "Tour por el Centro Histórico de Quito",
+        info: 
+            "Quito cuenta con cuatro puntos de información turística ubicados"+
+            "en lugares estratégicos para brindar ayuda a los visitantes a sacar"+
+            "el mayor provecho posible de su estadía en la ciudad y sus alrededores"
+    },
+    {
+        id: 2,
+        city: "Guayaquíl",
+        img: "./img/guayaquil.jpg",
+        name: "Tour por el Circuito del Estero Salado",
+        info: 
+            "Si necesita información turística sobre los atractivos que puede visitar"+
+            " en la ciudad, puede acercarse a las oficinas de la Empresa Pública Municipal"+
+            " de Turismo que trabaja en coordinación con la Muy Ilustre Municipalidad de"+
+            " Guayaquil, ubicada en la Calle 10 de Agosto y avenida Malecón Simón Bolívar, en el edificio Valra."
+    },
+    {
+        id: 3,
+        city: "Ambato",
+        img: "./img/ambato.jpg",
+        name: "Tour por la ruta de las 7 cochas",
+        info: 
+            "Ambato está ubicado en la Provincia de Tungurahua, es un moderno poema del "+
+            "desarrollo comercial y cultural, en una región con gracia natural."+
+            "Encuentra aquí la guía turística de Ambato"
+    },
+    {
+        id: 4,
+        city: "Cuenca",
+        img: "./img/cuenca.jpg",
+        name: "Tour por el Centro Histórico de Cuenca",
+        info: 
+            "Es la capital de la provincia del Azuay, está situada en la parte meridional de"+
+            " la cordillera andina ecuatoriana. Su Centro Histórico fue declarado Patrimonio "+
+            "de la Humanidad por la Unesco en el año 1999."
+    }
+]
